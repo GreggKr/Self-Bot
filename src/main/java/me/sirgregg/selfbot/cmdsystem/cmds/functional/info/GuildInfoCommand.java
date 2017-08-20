b0d100dce75140e0838b921a3e95226e31f5cc28
gregg <greggkrzykowski@gmail.com>
2017-08-20 18:58:40 -0500

    Fixed issue where url was null, causing the issue while URL encoding.

--- src/main/java/me/sirgregg/selfbot/cmdsystem/cmds/functional/info/GuildInfoCommand.java
+++ src/main/java/me/sirgregg/selfbot/cmdsystem/cmds/functional/info/GuildInfoCommand.java
@@ -28,33 +28,39 @@ public class GuildInfoCommand extends Command {
 		String key = configuration.getBitlyOAuthToken();
 
 		try {
-			String reqUrl = "https://api-ssl.bitly.com/v3/shorten?access_token=" + key + "&longUrl=" + URLEncoder.encode(e.getGuild().getIconUrl(), "UTF-8");
-			URLConnection urlConnection = new URL(reqUrl).openConnection();
-			urlConnection.connect();
+			String returnValue;
+			if (e.getGuild().getIconUrl() == null) {
+				returnValue = "None.";
+			} else {
+				String reqUrl = "https://api-ssl.bitly.com/v3/shorten?access_token=" + key + "&longUrl=" + URLEncoder.encode(e.getGuild().getIconUrl(), "UTF-8");
+				URLConnection urlConnection = new URL(reqUrl).openConnection();
+				urlConnection.connect();
 
-			StringBuilder jsonBuilder = new StringBuilder();
-			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-			String line;
-			while ((line = bufferedReader.readLine()) != null) {
-				jsonBuilder.append(line);
+				StringBuilder jsonBuilder = new StringBuilder();
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+				String line;
+				while ((line = bufferedReader.readLine()) != null) {
+					jsonBuilder.append(line);
+				}
+				String resJson = jsonBuilder.toString();
+
+				returnValue = "<" + new JsonParser().parse(resJson).getAsJsonObject().get("data").getAsJsonObject().get("url").getAsString() + ">";
+
+				if (returnValue.equals("")) {
+					e.getMessage().editMessage(EmbedUtil.createEmbed(color, "**Bad HTML status code.**\nStatus code: " + new JsonParser().parse(resJson).getAsJsonObject().get("status_code"))).queue();
+					return;
+				}
 			}
-			String resJson = jsonBuilder.toString();
 
-			String returnValue = new JsonParser().parse(resJson).getAsJsonObject().get("data").getAsJsonObject().get("url").getAsString();
-
-			if (returnValue.equals("")) {
-				e.getMessage().editMessage(EmbedUtil.createEmbed(color, "**Bad HTML status code.**\nStatus code: " + new JsonParser().parse(resJson).getAsJsonObject().get("status_code"))).queue();
-				return;
-			}
 			e.getMessage().editMessage(EmbedUtil.createEmbed(
 					color,
 					"**Information about Guild: **\n**Name: **" + e.getGuild().getName() + "\n" +
 							"**Owner: **" + e.getGuild().getOwner().getEffectiveName() + "#" + e.getGuild().getOwner().getUser().getDiscriminator() + "\n" +
 							"**Created On: **" + e.getGuild().getCreationTime().format(DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm:ss")) + "\n" +
 							"**# of Members: **" + e.getGuild().getMembers().size() + "\n" +
-							"**Download the Icon: **<" + returnValue + ">\n"
+							"**Download the Icon: **" + returnValue + "\n"
 			)).queue();
-		} catch (IOException ex) {
+		} catch (Exception ex) {
 			e.getMessage().editMessage(EmbedUtil.createEmbed(color, "**Caught Exception.**\nInvalid icon URL.")).queue();
 			return;
 		}
